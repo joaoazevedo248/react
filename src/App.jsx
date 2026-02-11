6932cf05206a4fc240decb6a26c2ed9f24a681e3
DEV <DEV@NSN1191261176.edu119.local>
2026-02-11 15:02:33 -0300

    aprendendo react com git

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+export function App() {
+
+  return (
+    <>
+    <h1>Olá mundo🗺️ </h1>
+    </>
+      
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './App.jsx'
+import './style/global.css'
+import './style/theme.css'
+
+//import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
